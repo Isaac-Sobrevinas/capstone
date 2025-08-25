0c0b117c7766c182ed7aa60367383e916716c421
Maowiii <maoritrixialeonardo@gmail.com>
2025-08-25 20:57:46 +0800

    - Add employee page

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -22,18 +22,20 @@ export const Input = ({
 }: InputProps) => {
   return (
     <div className={cn("", className)}>
-      {label && <p className="mb-1 text-sm font-medium">{label}</p>}
-      <input
+       <p className="mb-1 text-sm font-medium text-gray-500">{label}</p>
+       <input
         type={type}
         value={value}
         placeholder={placeholder}
+        readOnly={props.readOnly}
         className={cn(
           "border px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-black w-full",
           inputClassName,
-          error || hasError ? "border-red-500" : ""
+          error || hasError ? "border-red-500" : "",
+          props.readOnly ? "bg-gray-200 cursor-not-allowed" : ""
         )}
         {...props} // ✅ spread remaining props (onChange, onKeyDown, etc.)
-      />
+       />
       {error && <p className="text-red-500">{error}</p>}
     </div>
   );
